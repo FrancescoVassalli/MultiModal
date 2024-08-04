899ce3d86d86d6f8a35b89f238db11bd855649e4
francesco <francesco.vassalli@colorado.edu>
2024-08-03 20:31:04 -0600

    got a groq result for table info not function call

--- multi_snow/groq_call.py
+++ multi_snow/groq_call.py
@@ -4,7 +4,7 @@ import json
 import os
 
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"), )
-MODEL = 'llama3-groq-8b-8192-tool-use-preview'
+MODEL = 'llama3-groq-70b-8192-tool-use-preview'
 
 system_prompt = """You are a snowboarding coach who has recorded the following tricks in a markdown reference table. As this couch you always need to answer the question to best of your ability and follow the instructions closly in order to deliver the best response.  Your students will ask you some questions about diffferent tricks and you must  give helpful answers. If you want to demonstate a trick select one from the reference table. However if they ask about tricks only respond with the ones in the reference table here:  \nAthlete,Trick,Result,Unnamed: 3,ID\n----------,-----------------------------------------,---------,,66ae852d7b2deac81dd1286e\nCraig McMorris ,Impressive jump                \t,Success,,66ae852d7b2deac81dd1286e\nCraig McMorris ,Mid-air spin                   \t,Success,,66ae852d7b2deac81dd1286e\nCraig
 McMorris ,Massive jump                   \t,Success,,66ae852d7b2deac81dd1286e\nCraig McMorris ,Series of spins and flips      \t,Success,,66ae852d7b2deac81dd1286e\nCraig McMorris ,Massive jump                   \t,Success,,66ae852d7b2deac81dd1286e\nCraig McMorris ,Series of spins and flips      \t,Success,,66ae852d7b2deac81dd1286e\nCraig McMorris ,Massive jump                   \t,Success,,66ae852d7b2deac81dd1286e\nCraig McMorris ,Series of spins and flips
@@ -27,11 +27,7 @@ forty\t,Success,,66ae76647b2deac81dd1284a\n-----------,-------------------------
 
 def get_video(query:str):
     """Evaluate a mathematical expression"""
-    try:
-        result = eval(expression)
-        return json.dumps({"result": result})
-    except:
-        return json.dumps({"error": "Invalid expression"})
+    return query
 
 def run_conversation(user_prompt):
     messages=[
@@ -49,7 +45,7 @@ def run_conversation(user_prompt):
             "type": "function",
             "function": {
                 "name": "get_video",
-                "description": "Given a trick return the video if you do not know what trick to input consult the main table",
+                "description": "Given a trick return the video",
                 "parameters": {
                     "type": "object",
                     "properties": {
@@ -85,7 +81,7 @@ def run_conversation(user_prompt):
             function_to_call = available_functions[function_name]
             function_args = json.loads(tool_call.function.arguments)
             function_response = function_to_call(
-                expression=function_args.get("expression")
+                query=function_args.get("query")
             )
             messages.append(
                 {
@@ -102,5 +98,5 @@ def run_conversation(user_prompt):
         return second_response.choices[0].message.content
 
 #user_prompt = "What is a trick Shaun White did that others did not?"
-user_prompt = "Find a video of Shaun white doing a trick "
+user_prompt = "Using the provided table and not using any tools tell me the tricks that  Shaun white did"
 print(run_conversation(user_prompt))
