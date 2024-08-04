a6e3d87a72a6532a8dab8064a02d9a40b8a4b0e2
jeremy-london <jeremy.c.london@gmail.com>
2024-08-03 19:01:02 -0600

    feat: adds cors to app

--- api/app.py
+++ api/app.py
@@ -1,18 +1,18 @@
 import json
 import os
 from flask import Flask
+from flask_cors import CORS
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 
 app = Flask(__name__)
-
+CORS(app)  # Enable CORS for all routes
 
 @app.route("/")
 def hello_world():
     return json.dumps({"message": "Hello, World!"})
-
 
 if __name__ == "__main__":
     # Check if the FLASK_DEBUG environment variable is set to 'True'
